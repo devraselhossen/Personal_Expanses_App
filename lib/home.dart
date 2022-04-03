fd6475747d6eb0684909031af4e1fafbc1847780
rasel-hossen <straselhossen24@gmail.com>
2022-04-04 03:03:10 +0600

    five commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables, deprecated_member_use
 
+import 'package:add_product_demo/widget/new_transaction.dart';
+import 'package:add_product_demo/widget/transaction_list.dart';
 import 'package:flutter/material.dart';
 
-import 'widget/user_transaction.dart';
+import 'model/transaction.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,12 +14,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+        id: "t1", title: "New Bat", amount: 780.50, date: DateTime.now()),
+    Transaction(
+        id: "t2", title: "New Ball", amount: 120.50, date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: (){},
+            child: NewTransaction(_addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+            );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("My App"),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: () {
+                startAddNewTransaction(context);
+              },
+              icon: Icon(Icons.add))
+        ],
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -29,12 +68,14 @@ class _HomeState extends State<Home> {
               child: Text("Chart!"),
             ),
           ),
-          UserTransaction()
+          TransactionList(_userTransactions)
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          startAddNewTransaction(context);
+        },
         child: Icon(Icons.add),
       ),
     );
