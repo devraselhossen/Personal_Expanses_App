3acf7d7f8a4e583ca4f9c73e41c7899ca2ad3a05
rasel-hossen <straselhossen24@gmail.com>
2022-04-03 14:58:02 +0600

    second cimmite

--- lib/home.dart
+++ lib/home.dart
@@ -1,13 +1,23 @@
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
+
+import 'package:add_product_demo/transaction.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "t1", title: "New Bat", amount: 780.50, date: DateTime.now()),
+    Transaction(
+        id: "t2", title: "New Ball", amount: 70.50, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +26,20 @@ class _HomeState extends State<Home> {
       ),
       body: Column(
         children: [
-          
+          Container(
+            width: double.infinity,
+            child: Card(
+              elevation: 5,
+              child: Text("Chart!"),
+            ),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Text(tx.title),
+              );
+            }).toList()
+          )
         ],
       ),
     );
